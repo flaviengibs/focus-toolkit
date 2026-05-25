70b4df6bb074117bf4469000e57ae765a1142766
flaviengibs <149933385+flaviengibs@users.noreply.github.com>
2026-05-25 18:32:54 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,27 +1,7 @@
-// initialise une vérif toutes les 5 minutes
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.alarms.create("darkModeCheck", {
-    periodInMinutes: 5
+  chrome.storage.local.set({
+    todos: [],
+    notes: [],
+    savedTabs: []
   });
-});
-
-chrome.alarms.onAlarm.addListener(async () => {
-  const { enabled, auto } = await chrome.storage.local.get({
-    enabled: true,
-    auto: true
-  });
-
-  if (!enabled || !auto) return;
-
-  const hour = new Date().getHours();
-  const shouldEnable = hour >= 19 || hour < 7;
-
-  if (shouldEnable) {
-    const tabs = await chrome.tabs.query({});
-    for (let tab of tabs) {
-      if (tab.id) {
-        chrome.tabs.sendMessage(tab.id, { action: "enable" });
-      }
-    }
-  }
 });
